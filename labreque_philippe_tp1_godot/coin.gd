1ad8108afaa71bb8e866952e746584720a2bb6b6
Philippe La Breque <plb@gmail.com>
2025-09-05 15:52:47 -0400

    ajout exportation web

--- labreque_philippe_tp1_godot/coin.gd
+++ labreque_philippe_tp1_godot/coin.gd
@@ -1,17 +1,26 @@
 extends AnimatedSprite2D
 
+@onready var good_sound = $Bling
+@onready var bad_sound = $Wrong
+
+# Définir les frames "bon timing"
+var good_frames = [0, 2]  # Frame 2 et 4 (index 1 et 3)
+var score = 0
+func _ready() -> void:
+	play()  # Lancer l'animation en boucle
 
 
-func _on_pressed() -> void:
-	if frame == 2||4:
-		$Bling.play()
-		print("ok")
-	else :
-		$Wrong.stop()
-		print("not ok")
-
-
-func _on_ready() -> void:
+func _on_button_pressed() -> void:
+	if frame in good_frames:
+		good_sound.play()
+		print("🎯")
+		score+=1
 	
-	play()
-	pass # Replace with function body.
+	else:
+		bad_sound.play()
+		score+=-1
+		print("❌")
+
+
+func _process(delta: float) -> void:
+	$"../RichTextLabel".text=var_to_str(score)
